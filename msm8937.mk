229b637b17463ae2ec96db2f04da08ccf72a0b66
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-21 14:36:56 +0800

    CherishOS: msm8937-common: Make overlay for CherishOS

--- msm8937.mk
+++ msm8937.mk
@@ -416,6 +416,8 @@ PRODUCT_COPY_FILES += \
 
 # ROM: CherishOS
 ifneq ($(wildcard vendor/cherish),)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-cherish
+
 ifneq ($(DEVICE),tiare)
 WITH_GMS ?= true
 endif
