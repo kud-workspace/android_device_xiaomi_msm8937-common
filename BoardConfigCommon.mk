0086203756369c3560329635bd1ea1a1190ca841
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-10-29 09:27:15 +0800

    msm8937-common: Move BOARD_PLAT_PRIVATE sepolicy to SYSTEM_EXT_PRIVATE

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -162,7 +162,7 @@ endif
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 SELINUX_IGNORE_NEVERALLOWS := true
 
