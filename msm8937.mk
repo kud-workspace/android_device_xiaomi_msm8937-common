0ccfc608368fb8fc34cbf7e32a35744027fe062c
MrArtemSid <artem.sid.key@gmail.com>
2021-11-18 18:49:10 +0400

    Revert "msm8953-common: Switch to AOSP WFD"
    This reverts commit a327957eb79d5397d34092b457621a3f6d6763ba.

--- msm8937.mk
+++ msm8937.mk
@@ -417,5 +417,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Wi-Fi Display
+PRODUCT_PACKAGES += \
+    libnl
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/msm8937-common/msm8937-common-vendor.mk)
