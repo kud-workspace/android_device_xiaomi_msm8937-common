35a513a0fd841f972167d1181d05cc129b8cf7a3
MrArtemSid <artem.sid.key@gmail.com>
2021-11-19 21:26:32 +0400

    msm8937-common: Update QMI, CNE, Radio from Panther

--- extract-files.sh
+++ extract-files.sh
@@ -63,9 +63,6 @@ function blob_fixup() {
         product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml|product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
             sed -i 's/version="2.0"/version="1.0"/g' "${2}"
             ;;
-        vendor/lib64/libsettings.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
-            ;;
 	lib/libwfdmmsink.so)
             "${PATCHELF}" --add-needed "libshim_wfdmmsink.so" "${2}"
     esac
